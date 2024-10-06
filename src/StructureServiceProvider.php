9ae170bebd4d4e27431c74ed7334f21475261ca7
Rateb Alsaour <rateb.alasour.98@gmail.com>
2024-10-06 23:10:52 +0400

    v1.6 init

--- src/StructureServiceProvider.php
+++ src/StructureServiceProvider.php
@@ -12,7 +12,7 @@ use RatebSa\Structure\Console\Commands\MakeRepositoryAndDTO;
 class StructureServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services. v1.5
+     * Register any application services. v1.6
      * This method is for binding services, repositories, and other components into the service container.
      */
     public function register()
