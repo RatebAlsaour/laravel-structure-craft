ecc5133b21ff3d923fe1ca8ec0cdbc9499eae260
Rateb Alsaour <rateb.alasour.98@gmail.com>
2024-10-20 00:18:54 +0400

    resolve conflict

--- src/StructureServiceProvider.php
+++ src/StructureServiceProvider.php
@@ -12,7 +12,7 @@ use RatebSa\Structure\Console\Commands\MakeRepositoryAndDTO;
 class StructureServiceProvider extends ServiceProvider
 {
     /**
-     * Register any application services. v2.7
+     * Register any application services. v2.8
      *
      * This method is for binding services, repositories, and other components into the service container.
      */
